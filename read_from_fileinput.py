9d4dd1e193ad381b28f50c454e93ea93affb1d2e
xuexiaojun <xuexj@163.com>
2021-07-12 09:53:45 +0800

    修改格式

--- read_from_fileinput.py
+++ read_from_fileinput.py
@@ -1,10 +1,13 @@
 from __future__ import print_function
 import fileinput
 
-
 for line in fileinput.input():
-    meta = [fileinput.filename(), fileinput.fileno(), fileinput.filelineno(),
-            fileinput.isfirstline(), fileinput.isstdin()]
+    meta = [
+        fileinput.filename(),
+        fileinput.fileno(),
+        fileinput.filelineno(),
+        fileinput.isfirstline(),
+        fileinput.isstdin()
+    ]
     print(*meta, end="")
     print(line, end="")
-
