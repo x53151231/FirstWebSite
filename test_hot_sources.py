2bf97543b7d562e746dffcc97c2735df5bccab24
xuexiaojun <xuexj2021@163.com>
2021-07-31 16:30:57 +0800

    add test_hot_sources.py

--- test_hot_sources.py
+++ test_hot_sources.py
@@ -1,9 +1,13 @@
 #! /usr/bin/python
 #-*- coding:UTF-8 -*-
-
-
 from __future__ import print_function
 from collections import Counter
 
 
 c = Counter()
+with open('access.log') as f:
+    for line in f:
+        c[line.split()[6]] += 1
+
+
+print("Popular resource : {0}".format(c.most_common(10)))
